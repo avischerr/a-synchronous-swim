466507572a4b12703a5e9098cf0e7f1f0da9d610
Arielle Scherr <ariellescherr@gmail.com>
2020-09-05 12:32:47 -0500

    Updated httpHandler and spec file

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -10,11 +10,23 @@ module.exports.backgroundImageFile = path.join('.', 'background.jpg');
 let messageQueue = null;
 module.exports.initialize = (queue) => {
   messageQueue = queue;
+  console.log('messageQueue: ', queue)
 };
 
+const validMessages = ['left', 'right', 'up', 'down'];
+var getRandomCommand = (validMessages) => {
+  var choice = Math.floor(Math.random() * (5))
+  return validMessages[choice];
+}
+
+// Receives and routes all AJAX requests
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
+  console.log('module.exports.router res: ', res);
+  console.log('module.exports.router req: ', req);
   res.writeHead(200, headers);
+  res.write(getRandomCommand(validMessages));
   res.end();
   next(); // invoke next() at the end of a request to help with testing!
 };
+
